dcccd1d058cb7a79f8f8460d7a95d54ca6f5fb9b
Carlos Jiménez Domínguez <89859160+carjizo@users.noreply.github.com>
2023-09-02 10:46:16 -0500

    initial commit

---
+++ src/main/java/org/sisvir/cloudgateway/sisvircloudgateway/SisvirCloudgatewayApplication.java
@@ -0,0 +1,13 @@
+package org.sisvir.cloudgateway.sisvircloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisvirCloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisvirCloudgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sisvir/cloudgateway/sisvircloudgateway/SisvirCloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sisvir.cloudgateway.sisvircloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisvirCloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
